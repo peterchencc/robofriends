5c6fd8b912bdd86c5e98544344e24305d7ea526e
peterchencc <peterchencc@gmail.com>
2021-02-04 15:58:13 +0800

    Add Card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <img src="https://robohash.org/test?200x200" alt="robots" />
+      <div>
+        <h2>Peter Chen</h2>
+        <p>peterchen@gmail.com</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
