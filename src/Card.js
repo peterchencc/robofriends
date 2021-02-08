c641e24c1257ffc086bc50427f8d149568adac88
peterchencc <peterchencc@gmail.com>
2021-02-08 20:18:23 +0800

    Update card with robots data from robots.js

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({ name, email, id }) => {
   return (
     <div>
-      <img src="https://robohash.org/test?200x200" alt="robots" />
+      <img src={`https://robohash.org/${id}?200x200`} alt="robots" />
       <div>
-        <h2>Peter Chen</h2>
-        <p>peterchen@gmail.com</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   )
